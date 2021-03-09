9f7ad1e8306bb982a61897d538d0ea5eb148ed6a
yigittopoglu <51920532+yigittopoglu@users.noreply.github.com>
2021-03-09 15:24:16 -0500

    shimmer api

---
+++ Shimmer_Csharp_API_MODDED4/ShimmerCapture/Logging.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ShimmerAPI
+{
+    class Logging
+    {
+        private StreamWriter PCsvFile = null;
+        private String FileName;
+        private String Delimeter = ",";
+        private Boolean FirstWrite = true;
+
+        public Logging(String fileName, String delimeter){
+            Delimeter = delimeter;
+            FileName = fileName;
+            try
+            {
+                PCsvFile = new StreamWriter(FileName, false);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Save to CSV",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+	    }
+
+        volatile bool bRowInProgress = false;
+        volatile int bWriteMarkerNext = 0;
+
+        public void WriteMarker(int inp)
+        {
+            bWriteMarkerNext = inp;
+
+        }
+
+        
+
+        public void WriteData(ObjectCluster obj)
+        {
+            bRowInProgress = true;
+            if (FirstWrite)
+            {
+                WriteHeader(obj);
+                FirstWrite = false;
+            }
+            Double[] data = obj.GetData().ToArray();
+
+            PCsvFile.Write(System.DateTime.Now.Hour + ":"+ System.DateTime.Now.Minute+ Delimeter + System.DateTime.Now.Second+"."+System.DateTime.Now.Millisecond + Delimeter);
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                PCsvFile.Write(data[i].ToString() + Delimeter);
+            }
+            PCsvFile.WriteLine();
+
+            if (bWriteMarkerNext > 0)
+            { 
+                PCsvFile.Write("MRK"+bWriteMarkerNext + "\n");
+                bWriteMarkerNext = 0;
+            }
+
+
+            bRowInProgress = false;
+        }
+
+        private void WriteHeader(ObjectCluster obj)
+        {
+            ObjectCluster objectCluster = new ObjectCluster(obj);
+            List<String> names = objectCluster.GetNames();
+            List<String> formats = objectCluster.GetFormats();
+            List<String> units = objectCluster.GetUnits();
+            List<Double> data = objectCluster.GetData();
+            String deviceId = objectCluster.GetShimmerID();
+
+            for (int i = 0; i < data.Count; i++)
+            {
+                PCsvFile.Write(deviceId + Delimeter);
+            }
+            PCsvFile.WriteLine();
+            for (int i = 0; i < data.Count; i++)
+            {
+                PCsvFile.Write(names[i] + Delimeter);
+            }
+            PCsvFile.WriteLine();
+            for (int i = 0; i < data.Count; i++)
+            {
+                PCsvFile.Write(formats[i] + Delimeter);
+            }
+            PCsvFile.WriteLine();
+            for (int i = 0; i < data.Count; i++)
+            {
+                PCsvFile.Write(units[i] + Delimeter);
+            }
+            PCsvFile.WriteLine();
+        }
+
+        public void CloseFile()
+        {
+            PCsvFile.Close();
+        }
+    }
+}
