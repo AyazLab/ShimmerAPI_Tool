e9ca447c186936b428d3fb3c06cf44a8a1e1f294
Adrian Curtin <adrian.b.curtin@gmail.com>
2021-03-19 15:34:07 -0400

    Updated assembly information and renamed executable

--- ShimmerCapture_source/Properties/AssemblyInfo.cs
+++ ShimmerCapture_source/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ShimmerCapture")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("ShimmerCapture Tool")]
+[assembly: AssemblyDescription("Modified version of shimmer capture tool for Drexel Projects")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Shimmer")]
 [assembly: AssemblyProduct("ShimmerCapture")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.6.0")]
-[assembly: AssemblyFileVersion("0.6.0")]
+[assembly: AssemblyVersion("0.7.0")]
+[assembly: AssemblyFileVersion("0.7.0")]
